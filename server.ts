17e7a7486cc4c5cfdc270db85cd79fffbe1ced98
llegolasi <volmarcolorado@gmail.com>
2025-10-31 17:33:28 -0300

    Fix: remove server.ts causing circular dependency

--- server.ts
+++
@@ -1,9 +0,0 @@
-import app from './hono';
-
-const port = process.env.PORT || 3000;
-console.log(`🚀 Server running on http://localhost:${port}`);
-
-export default {
-  port: Number(port),
-  fetch: app.fetch,
-};
